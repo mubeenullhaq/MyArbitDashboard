6401dd6a35c919f79d878072dc1dc003276c662e
Mubeen Ul Haq <mubeenullhaq@gmail.com>
2023-07-03 09:04:57 +0500

    Transactions Reducer added to Redux store and Code formatted

--- src/store/store.js
+++ src/store/store.js
@@ -1,27 +1,27 @@
-import { applyMiddleware, combineReducers, compose,createStore,} from 'redux';
-import PostsReducer from './reducers/PostsReducer';
-import thunk from 'redux-thunk';
-import { AuthReducer } from './reducers/AuthReducer';
-import  PoolsReducer  from './reducers/PoolsReducer';
-import  ManageStakingsReducer  from './reducers/ManageStakingsReducer';
+import { applyMiddleware, combineReducers, compose, createStore } from "redux";
+import PostsReducer from "./reducers/PostsReducer";
+import thunk from "redux-thunk";
+import { AuthReducer } from "./reducers/AuthReducer";
+import PoolsReducer from "./reducers/PoolsReducer";
+import ManageStakingsReducer from "./reducers/ManageStakingsReducer";
+import TransactionsReducer from "./reducers/TransactionsReducer";
 
-import todoReducers from './reducers/Reducers';
+import todoReducers from "./reducers/Reducers";
 //import { reducer as reduxFormReducer } from 'redux-form';
 const middleware = applyMiddleware(thunk);
 
-const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const reducers = combineReducers({
-    posts: PostsReducer,
-    pools: PoolsReducer,
-    stakings: ManageStakingsReducer,
-    auth: AuthReducer,
-		todoReducers,
-	//form: reduxFormReducer,	
-	
+  posts: PostsReducer,
+  pools: PoolsReducer,
+  stakings: ManageStakingsReducer,
+  transactions: TransactionsReducer,
+  auth: AuthReducer,
+  todoReducers,
+  //form: reduxFormReducer,
 });
 
 //const store = createStore(rootReducers);
 
-export const store = createStore(reducers,  composeEnhancers(middleware));
+export const store = createStore(reducers, composeEnhancers(middleware));
