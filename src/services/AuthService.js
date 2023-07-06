bfbb184fe975a51ffd75b289fa98d08cd4e2e37f
Mubeen Ul Haq <mubeenullhaq@gmail.com>
2023-07-06 06:57:16 +0500

    Code formatted and local storage token name updated

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,95 +1,89 @@
-import axios from 'axios';
+import axios from "axios";
 import swal from "sweetalert";
-import {
-    loginConfirmedAction,
-    Logout,
-} from '../store/actions/AuthActions';
+import { loginConfirmedAction, Logout } from "../store/actions/AuthActions";
 
 export function signUp(name, email, password, repeat_password) {
-    //axios call
-    const postData = {
-        name,
-        email,
-        password,
-        repeat_password
-       // returnSecureToken: true,
-    };
-    const reqUrl = process.env.REACT_APP_MYARBIT_HostUrl +"user";
-    return axios.post(
-        reqUrl,
-        postData,
-    );
+  //axios call
+  const postData = {
+    name,
+    email,
+    password,
+    repeat_password,
+    // returnSecureToken: true,
+  };
+  const reqUrl = process.env.REACT_APP_MYARBIT_HostUrl + "user";
+  return axios.post(reqUrl, postData);
 }
 
 export function login(email, password) {
-    const postData = {
-        email,
-        password,
-        //returnSecureToken: true,
-    };
+  const postData = {
+    email,
+    password,
+    //returnSecureToken: true,
+  };
 
-    const reqUrl = process.env.REACT_APP_MYARBIT_HostUrl+ "user/login"
+  const reqUrl = process.env.REACT_APP_MYARBIT_HostUrl + "user/login";
 
-    return axios.post(
-        reqUrl,
-        postData,
-    );
+  return axios.post(reqUrl, postData);
 }
 
 export function formatError(errorResponse) {
-    switch (errorResponse) {
-        case 'User alredy registered':
-            //return 'Email already exists';
-            swal("Oops", "Email already exists", "error");
-            break;
-        case 'Invalid email or password':
-            //return 'Email not found';
-           swal("Oops", "Invalid Email or Password ", "error",{ button: "Try Again!",});
-           break;
-        case 'INVALID_PASSWORD':
-            //return 'Invalid Password';
-            swal("Oops", "Invalid Password", "error",{ button: "Try Again!",});
-            break;
-        case 'USER_DISABLED':
-            return 'User Disabled';
+  switch (errorResponse) {
+    case "User alredy registered":
+      //return 'Email already exists';
+      swal("Oops", "Email already exists", "error");
+      break;
+    case "Invalid email or password":
+      //return 'Email not found';
+      swal("Oops", "Invalid Email or Password ", "error", {
+        button: "Try Again!",
+      });
+      break;
+    case "INVALID_PASSWORD":
+      //return 'Invalid Password';
+      swal("Oops", "Invalid Password", "error", { button: "Try Again!" });
+      break;
+    case "USER_DISABLED":
+      return "User Disabled";
 
-        default:
-            return '';
-    }
+    default:
+      return "";
+  }
 }
 
-export function saveTokenInLocalStorage(tokenDetails) {
-    // tokenDetails.expireDate = new Date(
-    //     new Date().getTime() + tokenDetails.expiresIn * 1000,
-    // );
-    localStorage.setItem('userDetails', JSON.stringify(tokenDetails));
+export function saveTokenInLocalStorage(_userDetails) {
+  // _userDetails.expireDate = new Date(
+  //     new Date().getTime() + _userDetails.expiresIn * 1000,
+  // );
+  localStorage.setItem("jwt", JSON.stringify(_userDetails.idToken));
+  localStorage.setItem("userDetails", JSON.stringify(_userDetails.user));
 }
 export function runLogoutTimer(dispatch, timer, navigate) {
-    setTimeout(() => {
-        //dispatch(Logout(history));
-        dispatch(Logout(navigate));
-    }, timer);
+  setTimeout(() => {
+    //dispatch(Logout(history));
+    dispatch(Logout(navigate));
+  }, timer);
 }
 
 export function checkAutoLogin(dispatch, navigate) {
-    const tokenDetailsString = localStorage.getItem('userDetails');
-    let tokenDetails = '';
-    if (!tokenDetailsString) {
-        dispatch(Logout(navigate));
-		return;
-    }
+  const tokenDetailsString = localStorage.getItem("jwt");
+  let tokenDetails = "";
+  if (!tokenDetailsString) {
+    dispatch(Logout(navigate));
+    return;
+  }
 
-    tokenDetails = JSON.parse(tokenDetailsString);
-    let expireDate = new Date(tokenDetails.expireDate);
-    let todaysDate = new Date();
+  tokenDetails = JSON.parse(tokenDetailsString);
+  let expireDate = new Date(tokenDetails.expireDate);
+  let todaysDate = new Date();
 
-    if (todaysDate > expireDate) {
-        dispatch(Logout(navigate));
-        return;
-    }
-		
-    dispatch(loginConfirmedAction(tokenDetails));
-	
-    const timer = expireDate.getTime() - todaysDate.getTime();
-    runLogoutTimer(dispatch, timer, navigate);
+  if (todaysDate > expireDate) {
+    dispatch(Logout(navigate));
+    return;
+  }
+
+  dispatch(loginConfirmedAction(tokenDetails));
+
+  const timer = expireDate.getTime() - todaysDate.getTime();
+  runLogoutTimer(dispatch, timer, navigate);
 }
