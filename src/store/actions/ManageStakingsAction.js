1d87a5d4a79c55c4c2bea042038620274594216b
Mubeen Ul Haq <mubeenullhaq@gmail.com>
2023-07-06 19:05:17 +0500

    Actions for staking creation added

--- src/store/actions/ManageStakingsAction.js
+++ src/store/actions/ManageStakingsAction.js
@@ -1,3 +1,4 @@
+import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import {
   createPool,
   formatStakings,
@@ -8,13 +9,32 @@ import {
   updatePool,
 } from "../../services/ManageStakingsService";
 import {
-  CONFIRMED_CREATE_POOL_ACTION,
+  CONFIRMED_CREATE_STAKINGS_ACTION,
   CONFIRMED_GET_STAKINGS_ACTION,
   GET_STAKINGS_FAILED_ACTION,
   LOADING_TOGGLE_ACTION,
-  CONFIRMED_EDIT_POOL_ACTION,
-  CONFIRMED_DELETE_POOL_ACTION,
 } from "./ManageStakingTypes";
+
+//Action to create a staking of a Logged-In User
+export function createStakingsAction(_amount, _poolId, navigate) {
+  return (dispatch, getState) => {
+    createStaking(_amount, _poolId)
+      .then((response) => {
+        let staking = response.data.staking;
+        let updated_user = response.data.updated_user;
+        localStorage.setItem("userDetails", JSON.stringify(updated_user));
+        dispatch(confirmedCreateStakingsAction(true));
+        navigate("/manage-stakings");
+      })
+      .catch((error) => {
+        //console.log('error');
+        //console.log(error);
+        console.log(error);
+        const errorMessage = formatError(error.response.data);
+        dispatch(getStakingsFailedAction(errorMessage));
+      });
+  };
+}
 
 //Action for reading all in-process stakings of a Logged-In User
 export function getStakingsAction() {
@@ -52,10 +72,17 @@ export function getStakingsHistoryAction() {
   };
 }
 
-export function confirmedGetStakingsAction(stakings) {
+export function confirmedGetStakingsAction(flag) {
   return {
     type: CONFIRMED_GET_STAKINGS_ACTION,
-    payload: stakings,
+    payload: flag,
+  };
+}
+
+export function confirmedCreateStakingsAction(staking) {
+  return {
+    type: CONFIRMED_CREATE_STAKINGS_ACTION,
+    payload: staking,
   };
 }
 
