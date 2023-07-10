ad14c396a1b88dd080fb36348e2a061442c04694
Mubeen Ul Haq <mubeenullhaq@gmail.com>
2023-07-10 08:16:48 +0500

    Actions for pool creation added

--- src/store/actions/PoolsActions.js
+++ src/store/actions/PoolsActions.js
@@ -1,54 +1,39 @@
-import {
-    createPool,
-    formatPools,
-    getPools,
-    deletePool,
-    updatePool
-} from '../../services/PoolsService';
-import {
-    CONFIRMED_CREATE_POOL_ACTION,
-    CONFIRMED_GET_POOL_ACTION,
-    CONFIRMED_EDIT_POOL_ACTION,
-    CONFIRMED_DELETE_POOL_ACTION,
-} from './PoolsTypes';
+import { createPool, formatPools, getPools, deletePool, updatePool } from "../../services/PoolsService";
+import { CONFIRMED_CREATE_POOL_ACTION, CONFIRMED_GET_POOL_ACTION, CONFIRMED_EDIT_POOL_ACTION, CONFIRMED_DELETE_POOL_ACTION } from "./PoolsTypes";
+
+export function createPoolAction(_poolName, _duration, _profit) {
+  return (dispatch, getState) => {
+    createPool(_poolName, _duration, _profit)
+      .then((response) => {
+        dispatch(confirmedCreatePoolsAction(response.data));
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+}
 
 export function getPoolsAction() {
-    return (dispatch, getState) => {
-        getPools().then((response) => {
-            let pools = formatPools(response.data);
-            dispatch(confirmedGetPoolsAction(pools));
-        });
-    };
+  return (dispatch, getState) => {
+    getPools().then((response) => {
+      let pools = formatPools(response.data);
+      dispatch(confirmedGetPoolsAction(pools));
+    });
+  };
 }
 export function confirmedGetPoolsAction(pools) {
-    return {
-        type: CONFIRMED_GET_POOL_ACTION,
-        payload: pools,
-    };
+  return {
+    type: CONFIRMED_GET_POOL_ACTION,
+    payload: pools,
+  };
 }
 
-
-// export function createPostAction(postData, history) {
-   
-// 	return (dispatch, getState) => {
-//         createPost(postData).then((response) => {
-//             const singlePost = {
-//                 ...postData,
-//                 id: response.data.name,
-//             };
-//             dispatch(confirmedCreatePostAction(singlePost));
-//             history.push('/postpage');
-//         });
-//     };
-// }
-// export function confirmedCreatePostAction(singlePost) {
-	
-//     return {
-//         type: CONFIRMED_CREATE_POOL_ACTION,
-//         payload: singlePost,
-//     };
-// }
-
+export function confirmedCreatePoolsAction(pool) {
+  return {
+    type: CONFIRMED_CREATE_POOL_ACTION,
+    payload: pool,
+  };
+}
 
 // export function deletePostAction(postId, history) {
 //     return (dispatch, getState) => {
@@ -65,14 +50,13 @@ export function confirmedGetPoolsAction(pools) {
 //     };
 // }
 
-
 // export function updatePostAction(post, history) {
 //     return (dispatch, getState) => {
 //         updatePost(post, post.id).then((reponse) => {
 //             dispatch(confirmedUpdatePostAction(post));
 //             history.push('/postpage');
 //         });
-			
+
 //     };
 // }
 // export function confirmedUpdatePostAction(post) {
