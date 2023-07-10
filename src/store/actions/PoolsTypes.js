cb66e0a4ac94a801ea5b73b1188b48ca2a5e35de
Mubeen Ul Haq <mubeenullhaq@gmail.com>
2023-07-10 23:14:13 +0500

    action type to hide pool added

--- src/store/actions/PoolsTypes.js
+++ src/store/actions/PoolsTypes.js
@@ -1,12 +1,14 @@
 //export const CREATE_POOLS_ACTION = '[pools Action] Create pools';
-export const CONFIRMED_CREATE_POOL_ACTION = '[pools Action] Confirmed Create pools';
+export const CONFIRMED_CREATE_POOL_ACTION = "[pools Action] Confirmed Create pools";
 
 //export const GET_POOLSS = '[pools Action] Get POOLSs';
-export const CONFIRMED_GET_POOL_ACTION = '[pools Action] Confirmed Get POOLSs';
+export const CONFIRMED_GET_POOL_ACTION = "[pools Action] Confirmed Get POOLSs";
 
-export const CONFIRMED_GET_POOL_REQUEST_ACTION = '[pools Action] Confirmed Get Request POOLSs';
+export const CONFIRMED_GET_POOL_REQUEST_ACTION = "[pools Action] Confirmed Get Request POOLSs";
+
+export const CONFIRMED_HIDE_POOL_ACTION = "[pools Action] Confirmed Hide POOL";
 
 //export const EDIT_POOLS_ACTION = '[pools Action] Edit pools';
-export const CONFIRMED_EDIT_POOL_ACTION = '[pools Action] Confirmed Edit pools';
+export const CONFIRMED_EDIT_POOL_ACTION = "[pools Action] Confirmed Edit pools";
 
-export const CONFIRMED_DELETE_POOL_ACTION = '[pools Action] Confirmed Delete pools';
+export const CONFIRMED_DELETE_POOL_ACTION = "[pools Action] Confirmed Delete pools";
