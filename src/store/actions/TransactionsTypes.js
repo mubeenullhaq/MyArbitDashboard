55e000769a1ec773a748b94598c8cb6787e6f7c6
Mubeen Ul Haq <mubeenullhaq@gmail.com>
2023-07-12 13:14:28 +0500

    Added GET_WITHRAWAL_REQUEST type

--- src/store/actions/TransactionsTypes.js
+++ src/store/actions/TransactionsTypes.js
@@ -1,12 +1,14 @@
 //export const CREATE_POOLS_ACTION = '[transactions Action] Create pools';
-export const CONFIRMED_CREATE_TRANSACTIONS_ACTION = '[transactions Action] Confirmed Create Transactions';
+export const CONFIRMED_CREATE_TRANSACTIONS_ACTION = "[transactions Action] Confirmed Create Transactions";
 
 //export const GET_POOLSS = '[transactions Action] Get POOLSs';
-export const CONFIRMED_GET_TRANSACTIONS_ACTION = '[transactions Action] Confirmed Get Transactions';
+export const CONFIRMED_GET_TRANSACTIONS_ACTION = "[transactions Action] Confirmed Get Transactions";
 
-export const GET_TRANSACTIONS_FAILED_ACTION = '[transactions Action] Confirmed Get Request POOLSs';
+export const CONFIRMED_GET_WITHRAWAL_REQUEST_TRANSACTIONS_ACTION = "[transactions Action] Confirmed Get Withdrawl Request Transactions";
+
+export const GET_TRANSACTIONS_FAILED_ACTION = "[transactions Action] Confirmed Get Request POOLSs";
 
 //export const EDIT_POOLS_ACTION = '[transactions Action] Edit pools';
-export const LOADING_TOGGLE_ACTION = '[transactions Action] Confirmed Edit Transactions';
+export const LOADING_TOGGLE_ACTION = "[transactions Action] Confirmed Edit Transactions";
 
-export const CONFIRMED_DELETE_TRANSACTIONS_ACTION = '[transactions Action] Confirmed Delete Transactions';
+export const CONFIRMED_DELETE_TRANSACTIONS_ACTION = "[transactions Action] Confirmed Delete Transactions";
