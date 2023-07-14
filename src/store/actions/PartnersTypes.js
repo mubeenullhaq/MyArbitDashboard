233f07034eab4b4ddddb08dc80014d8af8ac10c6
Mubeen Ul Haq <mubeenullhaq@gmail.com>
2023-07-14 05:18:55 +0500

    Actions types for partner added

---
+++ src/store/actions/PartnersTypes.js
@@ -0,0 +1,6 @@
+//export const GET_POOLSS = '[pools Action] Get PARTNERS';
+export const CONFIRMED_GET_PARTNERS_ACTION = "[partners Action] Confirmed Get PARTNERS";
+
+export const CONFIRMED_UPDATE_PARTNER_ACTION = "[partners Action] Confirmed Update PARTNER";
+
+export const FAILED_GET_PARTNERS_ACTION = "[partners Action] Failed Get PARTNERS";
