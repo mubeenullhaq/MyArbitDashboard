8ae56c91a8554ce241cf802c753d78d1460b06b9
Mubeen Ul Haq <mubeenullhaq@gmail.com>
2023-08-02 17:19:45 +0500

    circular progress bar added

--- src/jsx/pages/ManageStakings.js
+++ src/jsx/pages/ManageStakings.js
@@ -3,8 +3,11 @@ import { connect, useDispatch } from "react-redux";
 import { getStakingsAction, unStakeAction, loadingToggleAction } from "../../store/actions/ManageStakingsAction";
 import { Dropdown, Button, Modal } from "react-bootstrap";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const ManageStakings = (props) => {
+  const percentage = 80;
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [unstakeModal, setUnstakeModal] = useState(false);
@@ -28,6 +31,46 @@ const ManageStakings = (props) => {
     e.preventDefault();
     dispatch(unStakeAction(unStakeDoc, navigate));
     setUnstakeModal(false);
+  };
+  const formatDate = (dateTimeString) => {
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+      timeZone: "UTC",
+    };
+    const formattedDate = new Date(dateTimeString).toLocaleString("en-US", options);
+    return formattedDate;
+  };
+  const calProgressbarValue = (_startDate_str, _EndDate_str) => {
+    const startDate = new Date(_startDate_str);
+    const endDate = new Date(_EndDate_str);
+
+    // Calculate time difference in milliseconds
+    const timeDiff = endDate - startDate;
+    // Convert milliseconds to days
+    const daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+    return daysLeft;
+  };
+
+  const calProgressbarText = (_startDate_str, _EndDate_str) => {
+    const startDate = new Date(_startDate_str);
+    const endDate = new Date(_EndDate_str);
+
+    // Calculate time difference in milliseconds
+    const timeDiff = endDate - startDate;
+    // Convert milliseconds to days
+    const daysLeft = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    if (daysLeft >= 30) {
+      const monthsLeft = Math.floor(daysLeft / 30);
+      return `${monthsLeft}m left`;
+    } else {
+      return `${daysLeft}d left`;
+    }
   };
   return (
     <Fragment>
@@ -103,6 +146,8 @@ const ManageStakings = (props) => {
                       <th scope="col">Profit</th>
                       <th scope="col">Amount Staked</th>
                       <th scope="col">Auto Stake</th>
+                      <th scope="col">Start Date</th>
+                      <th scope="col">Time Left</th>
                       <th scope="col">Action</th>
                     </tr>
                   </thead>
@@ -115,8 +160,38 @@ const ManageStakings = (props) => {
                           <td>{staking.pool_info.profit + "%"}</td>
                           <td>{staking.amount_staked}</td>
                           <td class={staking.auto_stake === true ? "text-success" : "text-danger"}>{staking.auto_stake == true ? "ON" : "OFF"}</td>
+                          <td>{formatDate(staking.created_at)}</td>
+                          <td style={{ width: 90, height: 90 }}>
+                            <CircularProgressbar
+                              value={calProgressbarValue(staking.created_at, staking.end_date)}
+                              maxValue={staking.pool_info.duration}
+                              text={calProgressbarText(staking.created_at, staking.end_date)}
+                              styles={buildStyles({
+                                // Rotation of path and trail, in number of turns (0-1)
+                                rotation: 0.25,
+
+                                // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+                                strokeLinecap: "butt",
+
+                                // Text size
+                                textSize: "16px",
+
+                                // How long animation takes to go from one percentage to another, in seconds
+                                pathTransitionDuration: 0.5,
+
+                                // Can specify path transition in more detail, or remove it entirely
+                                // pathTransition: 'none',
+
+                                // Colors
+                                pathColor: "#01a419",
+                                textColor: "#000",
+                                trailColor: "#d6d6d6",
+                                backgroundColor: "#3e98c7",
+                              })}
+                            />
+                          </td>
+                          {/* <td>{formatDate(staking.end_date)}</td>{" "} */}
                           <td>
-                            {" "}
                             <Button as="a" href="#" className="btn btn-primary mb-1 ms-1" onClick={() => openUnstakeModal(staking)}>
                               Un-Stake
                             </Button>
